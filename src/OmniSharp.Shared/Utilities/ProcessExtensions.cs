2795f02b40772b554f5fc44d5286dce259fcd585
eelgame <eelgame@qq.com>
2026-05-11 15:42:21 +0800

    fix: harden host process exit watching to reduce orphan servers

--- src/OmniSharp.Shared/Utilities/ProcessExtensions.cs
+++ src/OmniSharp.Shared/Utilities/ProcessExtensions.cs
@@ -7,65 +7,137 @@ namespace OmniSharp.Utilities
 {
     public static class ProcessExtensions
     {
+        private static readonly object s_gate = new object();
         private static Thread s_backgroundWatcher;
-        private static List<(Process process, Action action)> s_watchedProcesses;
+        private static List<WatchedProcess> s_watchedProcesses;
+
+        private sealed class WatchedProcess
+        {
+            public Process Process { get; }
+            public Action Action { get; }
+            public int Triggered;
+
+            public WatchedProcess(Process process, Action action)
+            {
+                Process = process;
+                Action = action;
+            }
+        }
 
         public static void OnExit(this Process process, Action action)
         {
-            var gate = new object();
-
-            void CleanUpProcesses()
+            if (process == null)
             {
-                lock (gate)
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var watchedProcess = new WatchedProcess(process, action);
+
+            lock (s_gate)
+            {
+                if (s_watchedProcesses == null)
                 {
-                    for (int i = s_watchedProcesses.Count - 1; i >= 0; --i)
-                    {
-                        var (p, a) = s_watchedProcesses[i];
-                        if (p.HasExited)
-                        {
-                            s_watchedProcesses.RemoveAt(i);
-                            a();
-                        }
-                    }
+                    s_watchedProcesses = new List<WatchedProcess>();
+                }
+
+                s_watchedProcesses.Add(watchedProcess);
+
+                if (s_backgroundWatcher == null)
+                {
+                    s_backgroundWatcher = new Thread(Watcher) { IsBackground = true };
+                    s_backgroundWatcher.Start();
                 }
             }
 
-            void Watcher()
+            TryAttachExitEvent(watchedProcess);
+            if (HasProcessExited(process))
             {
-                while (true)
-                {
-                    CleanUpProcesses();
+                Trigger(watchedProcess);
+            }
+        }
 
-                    // REVIEW: Configurable?
-                    Thread.Sleep(2000);
-                }
+        private static bool HasProcessExited(Process process)
+        {
+            try
+            {
+                return process.HasExited;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        private static void Trigger(WatchedProcess watchedProcess)
+        {
+            if (Interlocked.Exchange(ref watchedProcess.Triggered, 1) != 0)
+            {
+                return;
             }
 
-            if (PlatformHelper.IsMono)
+            lock (s_gate)
             {
-                // In mono 3.10, the Exited event fires immediately, we're going to poll instead
-                lock (gate)
+                s_watchedProcesses?.Remove(watchedProcess);
+            }
+
+            try
+            {
+                watchedProcess.Action();
+            }
+            catch
+            {
+            }
+        }
+
+        private static void TryAttachExitEvent(WatchedProcess watchedProcess)
+        {
+            try
+            {
+                watchedProcess.Process.EnableRaisingEvents = true;
+                watchedProcess.Process.Exited += (sender, e) => Trigger(watchedProcess);
+            }
+            catch
+            {
+                Trigger(watchedProcess);
+            }
+        }
+
+        private static void CleanUpProcesses()
+        {
+            WatchedProcess[] processes;
+
+            lock (s_gate)
+            {
+                if (s_watchedProcesses == null || s_watchedProcesses.Count == 0)
                 {
-                    if (s_watchedProcesses == null)
-                    {
-                        s_watchedProcesses = new List<(Process process, Action action)>();
-                    }
+                    return;
+                }
 
-                    s_watchedProcesses.Add((process, action));
+                processes = s_watchedProcesses.ToArray();
+            }
 
-                    if (s_backgroundWatcher == null)
-                    {
-                        s_backgroundWatcher = new Thread(Watcher) { IsBackground = true };
-                        s_backgroundWatcher.Start();
-                    }
+            foreach (var watchedProcess in processes)
+            {
+                if (HasProcessExited(watchedProcess.Process))
+                {
+                    Trigger(watchedProcess);
                 }
             }
-            else
+        }
+
+        private static void Watcher()
+        {
+            while (true)
             {
-                process.Exited += (sender, e) =>
-                {
-                    action();
-                };
+                CleanUpProcesses();
+
+                // REVIEW: Configurable?
+                Thread.Sleep(2000);
             }
         }
 
